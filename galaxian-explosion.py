cdbd152f84a4bf579e8da0e6688b438887cc12f7
shinkala <shinyokure@gmail.com>
2023-11-23 16:01:24 +0100

    Trying to fix the evolution of position

--- galaxian-explosion.py
+++ galaxian-explosion.py
@@ -3,23 +3,43 @@ import matplotlib.pyplot as plt
 
 gravitationnal_constant = 6.67430e-11
 
+
+
+# ______                                      _                                                 _                    
+# |  ___|                                    | |                                               | |                   
+# | |_ _   _ _ __   ___   _ __ __ _ _ __   __| | ___  _ __ ___    _ __   ___  ___    __   _____| |     __ _  ___ ___ 
+# |  _| | | | '_ \ / __| | '__/ _` | '_ \ / _` |/ _ \| '_ ` _ \  | '_ \ / _ \/ __|   \ \ / / _ \ |    / _` |/ __/ __|
+# | | | |_| | | | | (__  | | | (_| | | | | (_| | (_) | | | | | | | |_) | (_) \__ \_   \ V /  __/ |_  | (_| | (_| (__ 
+# \_|  \__,_|_| |_|\___| |_|  \__,_|_| |_|\__,_|\___/|_| |_| |_| | .__/ \___/|___( )   \_/ \___|_( )  \__,_|\___\___|
+#                                                                | |             |/              |/                  
+#                                                                |_|                                                 
+
+
+
+def velocity_of_star(i, number_of_star, position, masse, dt, v_i):
+    vec_rji = []
+    r_ji = []
+    u_ji = []
+    f_ji = []
+
+    for j in range(number_of_star):
+        if i != j:
+            vec_rji.append(position[j] - position[i])
+            r_ji.append(norm(vec_rji[j], 2))
+            u_ji.append((vec_rji[j])/(r_ji[j]))
+
+            f_ji.append(gravitationnal_constant * masse[j] * u_ji[j]/r_ji[j])
+
+    a_i = sum(f_ji)
+    v_i += v_i + dt * a_i
+    return v_i    
+
+
 def evolution_of_position():
-    
 
     for i in range(number_of_star):
-        for j in range(number_of_star):
-            if i !=  j:
-                
-                # Define here our relative position vector and the unitary vector to have the correct direction
 
-                vec_r_ji = position[j] - position[i]
-                r_ji = norm(r_ji, 2)
-                u_ji = vec_r_ji/r_ji
-
-                # Define for each particule j the force that he exerce on particule i
-
-                f_ji = (G * masse[j])/(r_ji**2) * u_ji
-
-            a_i = np.sum(f_ji)
+        
 
 
+# funcanimation.
